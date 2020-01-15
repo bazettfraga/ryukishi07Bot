6dc9e84344fe50b0521aa528390183b25046919c
Gabriel <gabrielplayroom@gmail.com>
2020-01-15 19:45:25 +0000

    jesus christ I want to kill myself
    yeah...

--- irc.py
+++ irc.py
@@ -5,14 +5,14 @@ import random
 channel = "#07Wonderland"
 server = "irc.swiftirc.net"
 nickname = "Ryukishi07"
-port = 6666
+
 
 irc = IRC()
-irc.connect(server, port, channel, nickname)
+irc.connect(server, channel, nickname)
 
 while 1:
     text = irc.get_text()
-    print text
+    print(text)
     if "PRIVMSG" in text and channel in text and "hello" in text:
         irc.send(channel, "Hello!")
 
--- irc_class.py
+++ irc_class.py
@@ -1,39 +1,29 @@
 import socket
 import sys
-import time
+
 
 class IRC:
- 
+
     irc = socket.socket()
   
-    def __init__(self):
-        # Define the socket
+    def __init__(self):  
         self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
- 
-    def send(self, channel, msg):
-        # Transfer data
-        self.irc.send(bytes("PRIVMSG " + channel + " " + msg + "\n", "UTF-8"))
- 
-    def connect(self, server, port, channel, botnick):
-        # Connect to the server
-        print("Connecting to: " + server)
-        self.irc.connect((server, port))
 
-        # Perform user authentication
-        self.irc.send(bytes("USER " + botnick + " " + botnick +" " + botnick + " :python\n", "UTF-8"))
-        self.irc.send(bytes("NICK " + botnick + "\n", "UTF-8"))
-        #self.irc.send(bytes("NICKSERV IDENTIFY " + botnickpass + " " + botpass + "\n", "UTF-8"))
-        time.sleep(5)
+    def send(self, chan, msg):
+        self.irc.send("PRIVMSG " + chan + " " + msg + "n")
 
-        # join the channel
-        self.irc.send(bytes("JOIN " + channel + "\n", "UTF-8"))
- 
-    def get_response(self):
-        time.sleep(1)
-         Get the response
-        resp = self.irc.recv(2048).decode("UTF-8")
- 
-        if resp.find('PING') != -1:                      
-            self.irc.send(bytes('PONG ' + resp.split().decode("UTF-8") [1] + '\r\n', "UTF-8")) 
- 
-        return resp
+    def connect(self, server, channel, botnick):
+        #defines the socket
+        print "connecting to:"+server
+        self.irc.connect((server, 6666))                                                         #connects to the server
+        self.irc.send("USER " + botnick + " " + botnick +" " + botnick + " :This is a fun bot!n") #user authentication
+        self.irc.send("NICK " + botnick + "n")               
+        self.irc.send("JOIN " + channel + "n")        #join the chan
+
+    def get_text(self):
+        text=self.irc.recv(2040)  #receive the text
+
+        if text.find('PING') != -1:                      
+            self.irc.send('PONG ' + text.split() [1] + 'rn') 
+
+        return text
