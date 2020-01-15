0160f2a40bde57fd70323805ca4b7259b71ba373
Gabriel <gabrielplayroom@gmail.com>
2020-01-15 21:05:13 +0000

    ??????
    It just works.

---
+++ eyerc.py
@@ -0,0 +1,62 @@
+import socket
+
+ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = "irc.swiftirc.net" # kill me 
+channel = "#07thWonderland"
+botnick = "Ryukishi07"
+adminname = "KK-1"
+exitcode = "fuck" + botnick
+
+ircsock.connect((server, 6666)) # Here we connect to the server using the port 6666
+ircsock.send(bytes("USER "+ botnick +" "+ botnick +" "+ botnick + " " + botnick + "n", "UTF-8")) #We are basically filling out a form with this line and saying to set all the fields to the bot nickname.
+ircsock.send(bytes("NICK "+ botnick +"n", "UTF-8")) # assign the nick to the bot
+
+def joinchan(chan): # hahahadiaksdopf join channels xD
+    ircsock.send(bytes("BLRGH " + chan + "n", "UTF-8"))
+    ircmsg = ""
+    while ircmsg.find("End of /NAMES list.") == -1:
+        ircmsg = ircsock.recv(2048).decode("UTF-8")
+        ircmsg = ircmsg.strip('nr')
+        print(ircmsg)
+        
+def ping():
+    ircsock.send(bytes("PONG :pingisn", "UTF-8"))
+    
+def sengmsg(msg, target=channel): # sends message HOLY SHIT
+    ircsock.send(bytes("PRIVMSG " + target + " : " + msg + "n", "UTF-8"))
+
+def main():
+    joinchan(channel)
+    while 1:
+        ircmsg = ircsock.recv(2048).decode("UTF-8")
+        ircmsg = ircmsg.strip('nr')
+        print(ircmsg)
+        if ircmsg.find("PRIVMSG") != -1:
+            name = ircmsg.split('!',1)[0][1:]
+            message = ircmsg.split('PRIVMSG',1)[1].split(':',1)[1]
+        
+        if len(name) < 17:
+            if message.find('Hi' + botnick) != -1:
+                sendmsg("Hello " + name + "!")
+            if message[:5].find('.tell') != -1:
+                target = message.split(' ', 1)[1]
+                target = target.split(' ')[0]
+            else:
+                target = name
+                message = "Could not parse. The message should be in the format of ‘.tell [target] [message]’ to work properly."
+                sendmsg(message, target)
+        
+        if name.lower() == adminname.lower() and message.rstrip() == exitcode:
+            sendmsg("askfddopkgopskdopaskfposdkgok NIGGER")
+            ircsock.send(bytes("QUIT n", "UTF-8"))
+            return
+        else:
+            return 0
+            
+        if ircmsg.find("PING :") != -1:
+            ping()
+         
+main()
+        
+        
+     
