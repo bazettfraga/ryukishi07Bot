7cfd9a6397a17f76213407af1f3c2958215ac61d
BazettFraga <holydaggertm@gmail.com>
2020-01-15 23:24:33 +0100

    testchan

--- irc.py
+++ irc.py
@@ -1,18 +1,56 @@
-from irc_class import *
-import os 
-import random
+import socket
+import sys
+import ssl
+import time
 
 channel = "#07thWonderland"
 server = "irc.swiftirc.net"
-nickname = "Ryukishi07"
+botnick = "Ryukishi09"
+port = 6666
+password = ""
+
+### Tail
+tail_files = [
+            '/tmp/file-to-tail.txt'
+            ]
+
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #defines the socket
+#irc = ssl.wrap_socket(irc_C)
+
+print("Establishing connection to [%s]" % (server))
+# Connect
+irc.connect((server, port))
+irc.setblocking(False)
+irc.send(bytes("USER "+ botnick +" "+ botnick +" "+ botnick +" :meLon-Test\n", encoding="UTF8"))
+irc.send(bytes("NICK "+ botnick +"\n", encoding="UTF8"))
+tail_line = []
+for i, tail in enumerate(tail_files):
+        tail_line.append('')
 
 
-irc = IRC()
-irc.connect(server, channel, nickname)
+while True:
+    time.sleep(2)
 
-while 1:
-    text = irc.get_text()
-    print(text)
-    if "PRIVMSG" in text and channel in text and "hello" in text:
-        irc.send(bytes(channel, "Hello!"))
+                    # Tail Files
+    for i, tail in enumerate(tail_files):
+        #try:
+            #f = open(tail, 'r')
+            #line = f.readlines()[-1]
+            #f.close()
+            #if tail_line[i] != line:
+                #tail_line[i] = line
+                #irc.send(bytes("PRIVMSG %s :%s" % (channel, line) ,encoding="UTF8"))
+        #except Exception as e:
+            #print ("Error with file %s" % (tail))
+            #print (e)
+        try:
+            text=str(irc.recv(2040))
+            print (text)
+            if text.find('PING') != -1:
+                irc.send(bytes('PONG ' + text.split() [1] + '\r\n' ,encoding="UTF8"))
+                irc.send(bytes("PRIVMSG nickserv :identify %s %s\r\n" % (botnick, password), encoding="UTF8"))
+                irc.send(bytes("JOIN "+ channel +"\n", encoding="UTF8"))
+        except Exception:
+            continue
 
+        
