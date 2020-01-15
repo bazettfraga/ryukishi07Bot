ba2050a0721a75f2805c14447b727e0d52efc89f
BazettFraga <holydaggertm@gmail.com>
2020-01-15 19:57:29 +0100

    commit main

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+import asyncio
+import configparser
+
+client = discord.Client()
+config = configparser.ConfigParser()
+config.read('config.ini')
+botToken = config['DISCORD']['botToken']
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+#@client.event
+#async def on_message(message):
+
+
+client.run(botToken)
